23d7e1f096195d747c043b8b3e616021b9bf2c6f
Paul Griffiths <mail@paulgriffiths.net>
2014-06-13 12:17:19 -0400

    Fix error in including non-dynamic header.

--- progs/unittests/test_main.cpp
+++ progs/unittests/test_main.cpp
@@ -12,6 +12,7 @@
  *  http://www.gnu.org/licenses/
  */
 
+#define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE "Unit tests for general ledger"
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
